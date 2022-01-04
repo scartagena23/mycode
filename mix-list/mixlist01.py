4751c1482d994668e4028f58f813e8fb129c8e78
Sheila Cartagena <scartagena23@users.noreply.github.com>
2022-01-04 21:54:08 +0000

    practicing mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# create list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# print the first item of the list (IP)
+print("The first item in the list (IP): " + my_list[0] )
+
+# print the second item of the list (port)
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# print the third item of the list (state)
+print("The last item in the list (state): " + my_list[2] )
+
+# create the ip list
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+
