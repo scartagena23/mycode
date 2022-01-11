4f8f0c7372f176405c53115b2ee4e0f7a58d1e91
Sheila Cartagena <scartagena23@users.noreply.github.com>
2022-01-11 21:08:27 +0000

    working with pandas

--- pandas/pandabear01.py
+++ pandas/pandabear01.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Alta3 Research | RZFeeser@alta3.com
-   Exploring using pandas to create dataframes, and output graphs"""
 
 import pandas as pd
+
+# create some graphs
+import matplotlib.pyplot as plt
 
 def main():
     # define the name of our xls file
@@ -20,20 +21,36 @@ def main():
     # Choose the first column "Title" as
     # index (index=0)
     movies_sheet1 = pd.read_excel(excel_file, sheet_name=0, index_col=0)
-
     # DF has 5 rows and 24 columns (indexed by title)
-    # print the top 10 movies in the dataframe
     print(movies_sheet1.head())
 
-    # export 5 movies from the top dataframe to excel
-    movies_sheet1.head(5).to_excel("5movies.xlsx")
+    # grab the next 2 sheets as well
+    movies_sheet2 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
+    # DF has 5 rows and 24 columns (indexed by title)
+    print(movies_sheet2.head())
 
-    # export 5 movies from the top of the dataframe to json
-    movies_sheet1.head(5).to_json("5movies.json")
+    movies_sheet3 = pd.read_excel(excel_file, sheet_name=2, index_col=0)
+    # DF has 5 rows and 24 columns (indexed by title)
+    print(movies_sheet3.head())
 
-    # export 5 movies from the top of the dataframe to csv
-    movies_sheet1.head(5).to_csv("5movies.csv")
+    # combine all DFs into a single DF called movies
+    movies = pd.concat([movies_sheet1, movies_sheet2, movies_sheet3])
 
+    # number of rows and columns (5042, 24)
+    print(movies.shape)
+
+    # sort DataFrame based on Gross Earnings
+    sorted_by_gross = movies.sort_values(["Gross Earnings"], ascending=False)
+
+    # Data is sorted by values in a column
+    # display the top 10 movies by Gross Earnings.
+    # passing the 10 values to head returns the top 10 not the default 5
+    print(sorted_by_gross.head(10))
+
+    # create a stacked bar graph
+    sorted_by_gross['Gross Earnings'].head(10).plot(kind="barh")
+    # save the figure as stackedbar.png
+    plt.savefig("stackedbar.png")
 
 if __name__ == "__main__":
     main()
